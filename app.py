490a48ec30c0043d44b9ea7409601add39830492
LiakhovichUladzislau <liakhovich.uladzislau@gmail.com>
2021-03-13 23:40:51 +0300

    Added 3,4 and 5 lines

--- app.py
+++ app.py
@@ -1,2 +1,5 @@
 str_ = "I’m planning to learn git conflicts resolution"
 print(str_)
+name = input("Please enter your name:")
+surname = input("Please enter your surname:")
+print("Hi, {} {}!".format(name, surname))
